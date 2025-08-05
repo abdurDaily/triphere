81511b75c0998be3f4d01275353f14506dc1d532
abdur <abdurdiary@gmail.com>
2025-08-05 21:10:38 +0600

    service

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,20 +24,20 @@ mobileLinks.forEach((link) => {
 });
 // * NAVIGATION END
 
-
 /**PRELOADER */
-window.addEventListener('load', () => {
-  const preloader = document.getElementById('preloader');
-  const mainContent = document.getElementById('main-content');
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("main-content");
   if (preloader && mainContent) {
-    preloader.style.opacity = '0';
-    preloader.style.pointerEvents = 'none';
+    preloader.style.opacity = "0";
+    preloader.style.pointerEvents = "none";
 
     setTimeout(() => {
       preloader.remove();
-      mainContent.style.display = 'block'; // show content after preloader is removed
+      mainContent.style.display = "block"; // show content after preloader is removed
     }, 600); // match fade out duration
   }
 });
 
 /**PRELOADER END */
+
